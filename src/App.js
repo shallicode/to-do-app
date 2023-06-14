f65371da3ce833f530a8ce636d83783efe493e9e
shallaki <shallaki@pop-os.localdomain>
2023-06-14 19:18:30 +0530

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className="text-center">Hello World</h1>
     </div>
   );
 }
