814e4f8cfec02df418bdb174a54376a6b65d064e
shallaki <shallaki@pop-os.localdomain>
2023-06-14 20:04:47 +0530

    Adding Styles and JSX

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">Hello World</h1>
+    <div className="container">
+      <div className="row">
+        <div className="todo-list">
+          <h1>Todo List</h1>
+          <ul className="list-unstyled">
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Complete first task
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Complete second task
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Complete third task
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
