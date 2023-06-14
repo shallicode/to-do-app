bf5a61ecc9f31c762cf70566f070ab301d65b579
shallaki <shallaki@pop-os.localdomain>
2023-06-14 20:20:44 +0530

    Updating JSX

--- src/App.js
+++ src/App.js
@@ -7,27 +7,42 @@ function App() {
       <div className="row">
         <div className="todo-list">
           <h1>Todo List</h1>
-          <ul className="list-unstyled">
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value="" />
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="todo-item-check-1"
+                />
+                <label className="form-check-label" htmlFor="todo-item-check-1">
                   Complete first task
                 </label>
               </div>
             </li>
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value="" />
+            <li className="list-group-item">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="todo-item-check-2"
+                />
+                <label className="form-check-label" htmlFor="todo-item-check-2">
                   Complete second task
                 </label>
               </div>
             </li>
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value="" />
+            <li className="list-group-item">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="todo-item-check-3"
+                />
+                <label className="form-check-label" htmlFor="todo-item-check-3">
                   Complete third task
                 </label>
               </div>
